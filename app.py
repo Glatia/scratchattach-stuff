ef456e1c72662d16a8e7393214d8ba254c76fdd1
Glatia <88858289+Glatia@users.noreply.github.com>
2024-05-25 14:47:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
-import scratchattach as scratch3
+import scratchattach as scratch
+from scratchattach import Session
+import time
 
-session = scratch3.Session(".eJxVj01PhDAQhv8LZxdptxTYmxyM0UTj166eyLSdLhVoCZSQrPG_2yZc9vo-M8-885ssM04WBkwOiZjcamEce5xhkZjcJN51aAOhpdZElUIiICtzDTwDDQx5XmlSCHJonnbDl5Cn9X56rT_k8f34-HmqH17gkgVN787G7swYTMU-rXjK05yEvIHFt01s0BgVICnynNGMBaR-wJ5d482AF2dju7sBJyPh9hnX5ttN3fV-C3MbhrKM0ZKCLJRgiksmKdWswHJfMZ1zXUnUBDmNpTzOXjrXmShfgxDVtVKADN_HXjFD68N1b5xNNzCnbzj2W1hvw3__171vEw:1s9Ums:yYA2TzIhVTSxb6kgxIFgMe95tl8", username="brownapplesauce")
+session = Session(".eJxVj01PhDAQhv8LZxdptxTYmxyM0UTj166eyLSdLhVoCZSQrPG_2yZc9vo-M8-885ssM04WBkwOiZjcamEce5xhkZjcJN51aAOhpdZElUIiICtzDTwDDQx5XmlSCHJonnbDl5Cn9X56rT_k8f34-HmqH17gkgVN787G7swYTMU-rXjK05yEvIHFt01s0BgVICnynNGMBaR-wJ5d482AF2dju7sBJyPh9hnX5ttN3fV-C3MbhrKM0ZKCLJRgiksmKdWswHJfMZ1zXUnUBDmNpTzOXjrXmShfgxDVtVKADN_HXjFD68N1b5xNNzCnbzj2W1hvw3__171vEw:1sAKDe:Un6EETGHLDY2S8Pv_wmARtmmGZ8", username="brownapplesauce")
 
-conn = session.connect_cloud("1025120117")
+connection = session.connect_cloud("1026857057")
 
-client = scratch3.CloudRequests(conn)
-
-@client.request
-def follower_number(username):
-    user = scratch3.get_user(username)
-    followers = user.follower_count()
-    print(followers)
-    conn.set_var("follower_count", followers)
-    return 0
+client = scratch.CloudRequests(connection)
 
 @client.request
-def message_number(username):
-    user = scratch3.get_user(username)
-    messages = user.message_count()
-    print(messages)
-    conn.set_var("message_count", messages)
-    return 0
+def follow(who, from_user):
+    
+    user = session.connect_user(who)
+    
+    user.follow()
+    
+    time.sleep(5)
+    
+    print(f"Now following {who}, courtesy of {from_user}")
+    
+    return "Done"
+
+@client.request
+def unfollow(who, from_user):
+    
+    user = session.connect_user(who)
+    
+    user.unfollow()
+    
+    time.sleep(5)
+    
+    print(f"Now unfollowing {who}, courtesy of {from_user}")
+    
+    return "Done"
 
 @client.event
 def on_ready():
-    print("Request handler is ready")
+    print("Client is connected")
 
 client.run()
